cf448e3f6bbfaf6971439765b93410d3d1041412
GabrielRuiseco19170012 <19170012@uttcampus.edu.mx>
2021-02-05 18:31:43 -0600

    First commit

---
+++ database/migrations/1612570515651_publication_schema.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class PublicationSchema extends Schema {
+  up () {
+    this.create('publications', (table) => {
+      table.increments()
+      table.integer('user_id').unsigned().references('id').inTable('users')
+      table.string('title', 80).notNullable()
+      table.string('content', 80).notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('publications')
+  }
+}
+
+module.exports = PublicationSchema
---
+++ database/migrations/1612570561916_commentary_schema.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CommentarySchema extends Schema {
+  up () {
+    this.create('commentaries', (table) => {
+      table.increments()
+      table.integer('publication_id').unsigned().references('id').inTable('publications')
+      table.integer('user_id').unsigned().references('id').inTable('users')
+      table.string('title', 80).notNullable()
+      table.string('content', 80).notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('commentaries')
+  }
+}
+
+module.exports = CommentarySchema
